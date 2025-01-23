6ca171d8d851d56c3bb4c9c52606c95593debc10
igor <59234118+goldibb@users.noreply.github.com>
2025-01-23 02:16:19 +0100

    working

--- ChaiBot/Program.cs
+++ ChaiBot/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NetCord;
+using NetCord.Hosting.Gateway;
+using NetCord.Hosting.Services;
+using NetCord.Hosting.Services.ApplicationCommands;
+using NetCord.Rest;
+using NetCord.Services.ApplicationCommands;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using ChaiBot;
+
+
+var builder = Host.CreateApplicationBuilder(args);
+
+// Dodaj konfigurację z appsettings.json
+// Zarejestruj TranslationService
+builder.Services.AddSingleton<TranslationService>();
+
+builder.Services
+    .AddDiscordGateway()
+    .AddApplicationCommands<ApplicationCommandInteraction, ApplicationCommandContext>();
+
+var host = builder.Build();
+
+// Dodaj komendy
+host.AddSlashCommand("ping", "Ping!", () => "Pong!")
+    .AddUserCommand("Username", (User user) => user.Username)
+    .AddMessageCommand("Length", (RestMessage message) => message.Content.Length.ToString());
+
+// Dodaj menu kontekstowe do tłumaczenia
+host.AddMessageCommand("Przetłumacz", async (RestMessage message, ApplicationCommandContext context) =>
+{
+    await message.ModifyAsync(properties => properties.Flags = MessageFlags.Ephemeral);
+    // Sprawdź, czy wiadomość nie jest od bota
+    if (message.Author.IsBot)
+    {
+        return "Nie mogę przetłumaczyć wiadomości od bota.";
+    }
+
+    // Pobierz TranslationService z kontenera DI
+    var translationService = host.Services.GetRequiredService<TranslationService>();
+
+    // Tłumacz wiadomość za pomocą MyMemory API
+    var translatedText = await translationService.TranslateTextAsync(message.Content, "en");
+
+    // Wyślij odpowiedź efemeryczną (widoczną tylko dla użytkownika)
+    return $"Przetłumaczone: {translatedText}";
+});
+
+host.AddModules(typeof(Program).Assembly);
+host.UseGatewayEventHandlers();
+
+await host.RunAsync();
--- ChaiBot/TranslationService.cs
+++ ChaiBot/TranslationService.cs
@@ -1,6 +1,43 @@
-﻿namespace ChaiBot;
+﻿using System.Text.Json;
+using Microsoft.Extensions.Configuration;
+
+namespace ChaiBot;
 
 public class TranslationService
 {
-    
+    private readonly IConfiguration _configuration;
+
+    public TranslationService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public async Task<string> TranslateTextAsync(string text, string targetLanguage)
+    {
+        using (var httpClient = new HttpClient())
+        {
+            // Odczytaj klucz API z konfiguracji
+            var apiKey = _configuration["MyMemoryApi:ApiKey"];
+            if (string.IsNullOrEmpty(apiKey))
+            {
+                throw new Exception("Klucz API MyMemory nie został znaleziony w konfiguracji.");
+            }
+
+            // Ustaw język źródłowy na polski (pl)
+            var sourceLanguage = "pl"; // Język źródłowy to zawsze polski
+
+            // Wyślij żądanie do MyMemory API
+            var url = $"https://api.mymemory.translated.net/get?q={Uri.EscapeDataString(text)}&langpair={sourceLanguage}|{targetLanguage}&key={apiKey}";
+            var response = await httpClient.GetStringAsync(url);
+
+            // Parsowanie odpowiedzi JSON
+            var translationResult = JsonDocument.Parse(response);
+            var translatedText = translationResult.RootElement
+                .GetProperty("responseData")
+                .GetProperty("translatedText")
+                .GetString();
+
+            return translatedText;
+        }
+    }
 }
